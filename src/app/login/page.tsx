d544d0998f3fa46f6e1b259d521f1c62165e3b90
Kie Ren <kieren.siew@gmail.com>
2024-04-12 15:51:34 +0800

    asdf

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,82 +1,88 @@
-"use client";
-import Image from "next/image";
-import {
-    Button,
-    ButtonGroup,
-    Tabs,
-    TabList,
-    TabPanels,
-    Tab,
-    TabIndicator,
-    Link,
-    TabPanel,
-    Checkbox,
-    Card,
-    Stack,
-    StackDivider,
-    CardHeader,
-    CardBody,
-    CardFooter,
-    Box,
-    Input,
-    Heading,
-    Text,
-    List,
-    ListItem,
-    ListIcon,
-    OrderedList,
-    UnorderedList,
-} from "@chakra-ui/react";
-import Navbar from "@/components/navbar";
-export default function Login() {
-    return (
-        <>
-            <Box display="flex" h="100vh" w="100vw" bg="#f3f5f8">
-                <Card variant="filled" margin="auto" bgColor="white" w="30%" boxShadow={'lg'}>
-                    <CardHeader display="flex">
-                        <Image
-                            height="50"
-                            width="50"
-                            alt="Oh no! The icon isn't loading!"
-                            src="/audionotesbgless.png"></Image>
-                        <Heading size="xl">Lecturise Login</Heading>
-                    </CardHeader>
+'use client'
 
-                    <CardBody>
-                        <Stack spacing="4">
-                            <Box>
-                                Username <br></br>
-                                <Input
-                                    mt="10px"
-                                    size="sm"
-                                    borderRadius="5px"
-                                    type="text"
-                                    background="#fff"></Input>
-                            </Box>
-                            <Box>
-                                Password <br></br>
-                                <Input
-                                    mt="10px"
-                                    size="sm"
-                                    borderRadius="5px"
-                                    type="password"
-                                    background="#fff"></Input>
-                            </Box>
-                            <Box display="flex">
-                                <Checkbox>Remember me</Checkbox>
-                                <Link ml="auto" color={"blue.400"}>
-                                    Forgot password?
-                                </Link>
-                            </Box>
-                            <Link href="/dashboard">
-                                <Button bg="#2b63d9" color="white">
-                                    Login
-                                </Button>
-                            </Link>
-                        </Stack>
-                    </CardBody>
-                </Card>
-            </Box>
-        </>
-    );
+import {
+  Flex,
+  Box,
+  FormControl,
+  FormLabel,
+  Input,
+  InputGroup,
+  HStack,
+  InputRightElement,
+  Stack,
+  Button,
+  Heading,
+  Text,
+  useColorModeValue,
+  Link,
+} from '@chakra-ui/react'
+import { useState } from 'react'
+import {FiEyeOff, FiEye} from "react-icons/fi"
+export default function Login() {
+  const [showPassword, setShowPassword] = useState(false)
+
+  return (
+    <Flex
+      minH={'100vh'}
+      align={'center'}
+      justify={'center'}
+      bg={useColorModeValue('gray.50', 'gray.800')}>
+      <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
+        <Stack align={'center'}>
+          <Heading fontSize={'4xl'} textAlign={'center'}>
+            Sign in
+          </Heading>
+          <Text fontSize={'lg'} color={'gray.600'}>
+            to Lecturise
+          </Text>
+        </Stack>
+        <Box
+          rounded={'lg'}
+          bg={useColorModeValue('white', 'gray.700')}
+          boxShadow={'lg'}
+          p={8}>
+          <Stack spacing={4}>
+            <HStack>
+              <Box>
+              </Box>
+            </HStack>
+            <FormControl id="email" isRequired>
+              <FormLabel>Email address</FormLabel>
+              <Input type="email" />
+            </FormControl>
+            <FormControl id="password" isRequired>
+              <FormLabel>Password</FormLabel>
+              <InputGroup>
+                <Input type={showPassword ? 'text' : 'password'} />
+                <InputRightElement h={'full'}>
+                  <Button
+                    variant={'ghost'}
+                    onClick={() => setShowPassword((showPassword) => !showPassword)}>
+                    {showPassword ? <FiEye></FiEye> : <FiEyeOff></FiEyeOff>}
+                  </Button>
+                </InputRightElement>
+              </InputGroup>
+            </FormControl>
+            <Stack spacing={10} pt={2}>
+              <Button
+                loadingText="Submitting"
+                size="lg"
+                bg={'blue.400'}
+                color={'white'}
+                _hover={{
+                  bg: 'blue.500',
+                }}>
+                Sign up
+              </Button>
+            </Stack>
+            <Stack pt={6}>
+              <Text align={'center'}>
+                Already a user? <Link color={'blue.400'}>Login</Link>
+              </Text>
+            </Stack>
+          </Stack>
+        </Box>
+      </Stack>
+    </Flex>
+  )
 }
