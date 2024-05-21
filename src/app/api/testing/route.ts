297590282e60e65d64d3cafe26f0cbff5920ee94
Kie Ren <kieren.siew@gmail.com>
2024-05-21 10:38:38 +0800

    welp

--- src/app/api/testing/route.ts
+++ src/app/api/testing/route.ts
@@ -1,13 +1,6 @@
-// import type { NextApiRequest, NextApiResponse } from "next";
-// import { NextResponse, NextRequest } from "next/server";
+import type { NextApiRequest, NextApiResponse } from "next";
+import { NextResponse, NextRequest } from "next/server";
 
-// type ResponseData = {
-//   message: string;
-// };
-
-// export async function GET(
-//   req: NextApiRequest,
-//   res: NextApiResponse<ResponseData>,
-// ) {
-//   return NextResponse.json({ message: "Hello, Next.js!" });
-// }
+export default function GET() {
+  return 0;
+}
