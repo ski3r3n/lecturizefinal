d6bbf48314f79ffe9b388185e5d6d466efd172e2
Kie Ren <kieren.siew@gmail.com>
2024-05-21 10:35:58 +0800

    comment out testing

--- src/app/api/testing/route.ts
+++ src/app/api/testing/route.ts
@@ -1,13 +1,13 @@
-import type { NextApiRequest, NextApiResponse } from "next";
-import { NextResponse, NextRequest } from "next/server";
+// import type { NextApiRequest, NextApiResponse } from "next";
+// import { NextResponse, NextRequest } from "next/server";
 
-type ResponseData = {
-  message: string;
-};
+// type ResponseData = {
+//   message: string;
+// };
 
-export async function GET(
-  req: NextApiRequest,
-  res: NextApiResponse<ResponseData>,
-) {
-  return NextResponse.json({ message: "Hello, Next.js!" });
-}
+// export async function GET(
+//   req: NextApiRequest,
+//   res: NextApiResponse<ResponseData>,
+// ) {
+//   return NextResponse.json({ message: "Hello, Next.js!" });
+// }
