484341797fa09be53a98bf0bd2f2ea30c95810c5
avner <avner.keinan@gmail.com>
2024-06-14 15:30:36 +0800

    Fixed bug where SideBarWIthHeader would appear outside of dashboard/**

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,8 +2,15 @@
 
 import { ChakraProvider, Box } from "@chakra-ui/react";
 import SidebarWithHeader from "@/components/SidebarWithHeader";
+import { useRouter } from "next/navigation";
+import { usePathname } from 'next/navigation'
 
 export function Providers({ children }: { children: React.ReactNode }) {
+  const pathname = usePathname();
+
+  // Check if the current pathname starts with /dashboard
+  const isDashboard = pathname.startsWith("/dashboard");
+
   return (
     <ChakraProvider>
       <Box
@@ -16,7 +23,11 @@ export function Providers({ children }: { children: React.ReactNode }) {
         overflow="scroll"
         bgColor="#f3f5f8"
       >
-        <SidebarWithHeader>{children}</SidebarWithHeader>
+        {isDashboard ? (
+          <SidebarWithHeader>{children}</SidebarWithHeader>
+        ) : (
+          children
+        )}
       </Box>
     </ChakraProvider>
   );
